48a22b43d43f8fc2e0ba912596d63fbfb7406d92
eigenlambda123 <rmvilla987@gmail.com>
2026-03-06 15:27:31 +0800

    feat: added corsmiddleware configuration so that the app can run locally

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from database import create_db_and_tables
 
+from routers import auth
+
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth.router)
 
 @app.on_event("startup")
 def on_startup():
